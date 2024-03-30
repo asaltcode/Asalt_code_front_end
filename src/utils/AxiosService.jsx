67b17fcd514a536c6256a7841ff884eaeb53d77c
asaltcode <elangovanr558@gmail.com>
2024-03-30 12:31:47 +0530

    upadate

--- src/utils/AxiosService.jsx
+++ src/utils/AxiosService.jsx
@@ -1,7 +1,7 @@
 import axios from "axios"
 const AxiosService = axios.create({
     // baseURL: "http://localhost:8000",
-    baseURL: "/api",
+    baseURL: "https://asalt-code-back-end-d9ge.vercel.app/api",
     // baseURL: "https://asalt-code-back-end-2.onrender.com",
     // headers: {"Content-Type" : "application/json"},
 
